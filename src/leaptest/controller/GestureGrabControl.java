8d91af140f1f94203c9af37d3f8c963a596177c5
Annet Meijers <annet_44@hotmail.com>
2013-12-18 13:33:57 +0100

    Een beginnetje.

--- src/leaptest/controller/GestureGrabControl.java
+++ src/leaptest/controller/GestureGrabControl.java
@@ -4,10 +4,57 @@
  */
 package leaptest.controller;
 
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Hand;
+import com.leapmotion.leap.HandList;
+
 /**
  *
  * @author Annet
  */
-public class GestureGrabControl {
+public class GestureGrabControl extends LeapControl {
+    
+    private final static int HAND_PALM_THRESHOLD = 1;
+    private Frame frame;
+    private Hand previousHand;
+    private Controller leap;
+    private boolean isRightHanded = true;
+
+    public GestureGrabControl(Controller leap)
+    {
+        super(leap);
+        this.leap = leap;
+    }
+    
+    @Override
+    public void update(float tpf) {
+        HandList hands = frame.hands();
+        Hand hand = getGrabHand(hands);
+        if(previousHand != null)
+        {
+            if(hand == null)
+                return;
+            if(Math.abs(hand.palmPosition().getX()-previousHand.palmPosition().getX())<HAND_PALM_THRESHOLD &&
+                    Math.abs(hand.palmPosition().getY()-previousHand.palmPosition().getY())<HAND_PALM_THRESHOLD &&
+                    Math.abs(hand.palmPosition().getZ()-previousHand.palmPosition().getZ())<HAND_PALM_THRESHOLD)
+            {
+                
+            }
+        }
+        previousHand = hand;
+    }
+
+    @Override
+    protected void onFrame(Controller leap) {
+        frame = controller.frame();
+    }
+    
+    private Hand getGrabHand(HandList hands)
+    {
+        if (isRightHanded)
+            return hands.rightmost();
+        return hands.leftmost();
+    }
     
 }
