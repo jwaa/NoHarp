e5175eeb9d0a47b49fc6d8b794d3ccd4b15dffe4
jweijkamp <jweijkamp@hotmail.com>
2014-01-09 00:23:28 +0100

    Task manager geupdate: lijst gesorteerd. En export.model verwijdert.

--- src/leaptest/model/TaskManager.java
+++ src/leaptest/model/TaskManager.java
@@ -6,14 +6,19 @@ package leaptest.model;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  *
+ * TODO
+ * export eruit halen in map
  * @author Janne Weijkamp
  */
 public class TaskManager {
     
     private ArrayList <BlockModel> bm;
+    private int counter;
     
     public TaskManager(String path)
     {
@@ -22,34 +27,44 @@ public class TaskManager {
         File folder = new File(path);
         File[] listOfFiles = folder.listFiles();
         
+        Arrays.sort(listOfFiles);
+        String[] listOfFileNames = new String [listOfFiles.length];
+        
+        for(int i=0;i<listOfFiles.length;i++)
+        {   
+            listOfFileNames[i] = listOfFiles[i].getName();
+        }
+        Arrays.sort(listOfFileNames, new LengthCompare());
+        
         for(int i=0;i<listOfFiles.length; i++)
         {
-            bm.add(new BlockModel(path + listOfFiles[i].getName()));
+            bm.add(new BlockModel(path + listOfFileNames[i]));
+            System.out.println(path + listOfFileNames[i]);
         }
-        
-       
-    
     }
-    
-    // private function lijstje sorteren Collection.sort
-    // export eruit halen in map
-    
-    
-    
+
     /**
-     * volgende task starten
+     * Start next task
      */
     public void nextTask()
     {
-        
+        counter++;
     }
     
     /**
-     * teruggeven welke task die nu aan t doen is
+     * Return number of current task
      * @return 
      */
     public int getTask() 
     {
-        return 0; //TODO fix
+        return counter;
+    }
+ 
+    private static class LengthCompare implements Comparator<String>
+    {
+        public int compare(String s1, String s2)
+        {
+            return (s1.length() - s2.length());
+        }
     }
 }
