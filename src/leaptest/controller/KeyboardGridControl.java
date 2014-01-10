d3fcebc0a6c55ca1e9503f51e08a1326735a074d
Jasper <jaspervanderwaa@gmail.com>
2014-01-10 16:39:04 +0100

    + Made KeyBoardGridControl loggable
    Logs in KeyboardGridControl
    + KeyPressLeft
    + KeyPressRight

--- src/leaptest/controller/KeyboardGridControl.java
+++ src/leaptest/controller/KeyboardGridControl.java
@@ -10,18 +10,24 @@ import com.jme3.input.controls.AnalogListener;
 import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.FastMath;
 import leaptest.model.Grid;
+import leaptest.utils.Log;
+import leaptest.utils.Loggable;
 
 /**
  *
  * @author srw-install
  */
-public class KeyboardGridControl implements AnalogListener, Updatable {
+public class KeyboardGridControl implements AnalogListener, Updatable, Loggable {
 
     private Grid grid;
     
     private final float delta = FastMath.PI/1.5f;
     
     private float inc;
+    
+    // Log data
+    private boolean isKeyPressLeft;
+    private boolean isKeyPressRight;
     
     public KeyboardGridControl(InputManager inputManager, Grid grid)
     {
@@ -37,15 +43,31 @@ public class KeyboardGridControl implements AnalogListener, Updatable {
     }
 
     public void onAnalog(String name, float value, float tpf) {
-          if (name.equals("Rotate Grid Left"))
+        if (name.equals("Rotate Grid Left"))
+        {
+            isKeyPressLeft = true;
             inc += delta * tpf;
+        }
         else if (name.equals("Rotate Grid Right"))
+        {
+            isKeyPressRight = true;
             inc -= delta * tpf;
+        }
     }
 
     public void update(float tpf) {
         grid.rotate(inc);
         inc = 0;
+    }
+
+    public void log(Log log) {
+        if(isKeyPressLeft)
+            log.addEntry(Log.EntryType.KeyPressLeft, Boolean.toString(isKeyPressLeft));
+        if(isKeyPressRight)
+            log.addEntry(Log.EntryType.KeyPressRight, Boolean.toString(isKeyPressRight));
+        
+        isKeyPressLeft = false;
+        isKeyPressRight = false;
     }
     
 }
